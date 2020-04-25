29288c6f66f02bf1fb174fcc534331f4c3f2d79f
Darwin <darwin.xpanse@gmail.com>
2020-04-25 13:25:02 +0800

    design minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,37 @@ module.exports = {
         '10px': '0.625rem'
       },
       padding: {
-        '3px': '0.188rem'
+        '3px': '0.188rem',
+        '6px': '0.375rem'
       },
       spacing: {
         '240px': '15rem',
         '39px': '2.438rem',
         '165px': '10.333rem',
-        '112px': '7rem'
+        '112px': '7rem'  
       },
       colors: {
         'dark-blue': '#122344'
       },
       fontSize: {
         'mbl-srch-btn': "0.867rem",
-        'category-header': "1.067rem"
+        'category-header': "1.067rem",
+        'category-lists': "0.867rem"
       },
       borderWidth: {
         '3': '3px'
+      },
+      screens: {
+        '7xl': '1280px',
+        '208px': '13rem'
+      },
+      inset: {
+        '1/6': '-.5%',
+        '2/6': '16.2%',
+        '3/6': '33.33%',
+        '4/6': '50%',
+        '5/6': '67%',
+        '6/6': '84%'
       }
     }
   },
