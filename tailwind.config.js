899745ddb2ce7bd37be054f75d308083ad17820f
Darwin <darwin.xpanse@gmail.com>
2020-04-28 11:09:44 +0800

    pointers

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ module.exports = {
         '39px': '2.438rem',
         '165px': '10.333rem',
         '112px': '7rem',  
-        '342px': '21.375rem'
+        '342px': '25vw'
+
       },
       colors: {
         'dark-blue': '#122344',
@@ -35,7 +36,8 @@ module.exports = {
       },
       screens: {
         '7xl': '1280px',
-        '208px': '13rem'
+        '208px': '13rem',
+        'x-md': '790px'
       },
       inset: {
         '1/6': '-.5%',
@@ -44,10 +46,17 @@ module.exports = {
         '4/6': '50%',
         '5/6': '67%',
         '6/6': '84%',
-        'active': '-1.675rem'
+        'active': '-1.675rem',
+        'filter-top': '32px',
+        'filter-right': '-100%',
+        'cat-bot-bef': '97%',
+        'size-bot-bef': '98%',
+        'col-bot-bef': '99.3%',
+        'ran-bot-bef': '95%'
+
       },
       maxWidth: {
-        '342px': '21.375rem'
+        '342px': '25vw'
       }
     }
   },
